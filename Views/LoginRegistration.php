54df57930be824dbefc8607f617dc1595d741af2
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2024-04-19 21:58:49 +0600

    Add files via upload

---
+++ Views/LoginRegistration.php
@@ -0,0 +1,90 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
+  <link rel="stylesheet" href="../CSS/LoginRegistration.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <style>
+    .hidden {
+      display: none;
+    }
+  </style>
+</head>
+<body>
+  <div class="wrapper">
+    <h2>Login</h2>
+    <form method="post" action="../Controllers/logCheckController.php">
+      <div class="input-box">
+        <input type="text" placeholder="User Name" required name="user">
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Password" required name="pass">
+      </div>
+      <div class="remember-forgot">  <label><input type="checkbox">Remember Me</label>
+        <a id="forgot" href="#" class="fgt">Forgot Password?</a>
+      </div>
+      <button class="btn" name="log">LogIn</button>
+    </form>
+    <div class="register-link">
+      Don't have an account?
+      <button id="registerBtn">Register Now</button>
+    </div>
+    
+    <?php 
+      if(!empty($_SESSION['mess'])) {
+        echo $_SESSION['mess'];
+        unset($_SESSION['mess']);
+      }
+      if(!empty($_SESSION['reg'])) {
+        echo $_SESSION['reg'];
+        unset($_SESSION['reg']);
+      }
+    ?>
+  </div>
+
+  <div class="wrapper2 register-form hidden">
+    <h2>Register Now</h2>
+    <form method="POST" action="../Controllers/registrationController.php">
+      <div class="input-box">
+        <input type="text" placeholder="Enter Your User Name" required name="userName">
+    </div>
+    <div class="input-box">
+        <input type="e-mail" placeholder="Enter Your E-mail" required name="mail">
+    </div>
+    <div class="input-box">
+        <input type="text" placeholder="Create a Password" required name="pass">
+    </div>
+    <div class="input-box">
+        <input type="text" placeholder="Re-type your Password" required name="Rpass">
+    </div>
+      <button class="btn" name="log">Complete Registration</button>
+    </form>
+    <div class="register-link">
+      Already have an account?
+      <button id="loginBtn">Login Now</button>
+    </div>
+  </div>
+
+  <script>
+    document.getElementById("registerBtn").addEventListener("click", function() {
+      // Hide the login form
+      document.querySelector(".wrapper").classList.add("hidden");
+      // Show the register form
+      document.querySelector(".register-form").classList.remove("hidden");
+    });
+
+    // Add event listener to go back to login form
+    document.getElementById("loginBtn").addEventListener("click", function() {
+      // Hide the register form
+      document.querySelector(".register-form").classList.add("hidden");
+      // Show the login form
+      document.querySelector(".wrapper").classList.remove("hidden");
+    });
+  </script>
+</body>
+</html>
---
+++ Views/RestaurentPage.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RestaurentPage</title>
+    <link rel="stylesheet" href="../CSS/RestaurentPage.css">
+</head>
+
+<body>
+    <nav id="navbar">
+        <div id="logo">
+            <img src="../img/Restaurent_Pic/logo.png" alt="">
+        </div>
+
+        <ul>
+            <li class="item"><a href="#">At the Airport</a></li>
+            <li class="item"><a href="#">Flights</a></li>
+            <li class="item"><a href="#">Booking</a></li>
+            <li class="item"><a href="#">Help</a></li>
+            <li class="item"><a href="#">Reward</a></li>
+        </ul>
+    </nav>
+    <hr>
+
+    <section id="home">
+        <h3 class="h-primary">XTON : Welcome to XTON / At the Airport/RESTAURENT</h3>
+        <h1>Restaurent at XTON</h1>
+    </section>
+
+    <!-- <section id="home2"> -->
+        <section class="bigbox">
+        <h1 class="h-primary"></h1>
+        <div id="terminal">
+            <div class="terminalbox">
+                <h1>Select a Terminal</h1>
+                 <button class="btn2">Reset</button>
+                 <button class="btn2">Reset</button>
+                 <button class="btn2">Reset</button>
+                 <button class="btn2">Reset</button>
+            </div>
+
+            <div class="terminalbox1">
+                <button class="btn2">Reset</button>
+            </div>           
+        </div>
+        
+        <div id="terminal">
+            <div class="terminalbox2">
+                <h1> Select a Category</h1>
+            </div>
+        </div>
+        
+    <!-- </section> -->
+
+        <section class="shop selection">
+        <h1 class="h-primary"></h1>
+        <div id="shop">
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Bigsmoke.jpeg" alt="">
+                <button class="btn">View Details</button>
+            </div>
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Black Sheep Coffee.jpeg" alt="">
+                <button class="btn">View Details</button>
+            </div>
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Cavier House.jpg" alt="">
+                <button class="btn">View Details</button>
+            </div>
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Girraffe.png" alt="">
+                <button class="btn">View Details</button>
+        </div>
+        </section>
+
+        <section class="shop selection">
+        <h1 class="h-primary"></h1>
+        <div id="shop">
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Bigsmoke.jpeg" alt="">
+                <button class="btn">View Details</button>
+            </div>
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Black Sheep Coffee.jpeg" alt="">
+                <button class="btn">View Details</button>
+            </div>
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Cavier House.jpg" alt="">
+                <button class="btn">View Details</button>
+            </div>
+            <div class="box">
+                <img src="../img/Restaurent_Pic/Girraffe.png" alt="">
+                <button class="btn">View Details</button>
+        </div>
+        </section>
+    </section>
+
+    <hr>
+    <nav id="navbar2">
+        <div id="logo2">
+            <img src="../img/Restaurent_Pic/logo.png" alt="">
+              <!-- <h2>XTON Airport Limited</h2> -->
+            <hr>
+        </div>
+
+        <div id="logo3">
+            <img src="../img/Restaurent_Pic/up-arrow.png" alt="">
+            
+        </div>
+        
+
+        <!-- <ul>
+            <li class="item"><a href="#">At the Airport</a></li>
+            <li class="item"><a href="#">Flights</a></li>
+            <li class="item"><a href="#">Booking</a></li>
+            <li class="item"><a href="#">Help</a></li>
+            <li class="item"><a href="#">Reward</a></li>
+        </ul> -->
+    </nav>
+    
+</body>
+</html>
