78d7d2d0012f12618287c10426c22299f9eee8fa
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2024-04-20 13:03:28 +0600

    Update RestaurentPage.php

--- Views/RestaurentPage.php
+++ Views/RestaurentPage.php
@@ -5,9 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RestaurentPage</title>
     <link rel="stylesheet" href="../CSS/RestaurentPage.css">
-</head>
+      <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head> 
 
 <body>
+    <div class="nav1">
+        <i class="fa-solid fa-circle-exclamation"></i> M25 Junction 10-11 Closure: 15-18 March
+    </div>
     <nav id="navbar">
         <div id="logo">
             <img src="../img/Restaurent_Pic/logo.png" alt="">
@@ -20,6 +25,16 @@
             <li class="item"><a href="#">Help</a></li>
             <li class="item"><a href="#">Reward</a></li>
         </ul>
+        <div class="nav2-searchBar">
+          <input type="text" placeholder="search here" />
+          <div class="search-icon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+
+        <div class="nav2-login">
+         <a href="../Views/LoginRegistration.php"><span><i class="fa-solid fa-right-to-bracket"></i></span></a>
+        </div>
     </nav>
     <hr>
 
@@ -27,31 +42,31 @@
         <h3 class="h-primary">XTON : Welcome to XTON / At the Airport/RESTAURENT</h3>
         <h1>Restaurent at XTON</h1>
     </section>
-
-    <!-- <section id="home2"> -->
-        <section class="bigbox">
-        <h1 class="h-primary"></h1>
-        <div id="terminal">
-            <div class="terminalbox">
-                <h1>Select a Terminal</h1>
-                 <button class="btn2">Reset</button>
-                 <button class="btn2">Reset</button>
-                 <button class="btn2">Reset</button>
-                 <button class="btn2">Reset</button>
-            </div>
-
-            <div class="terminalbox1">
-                <button class="btn2">Reset</button>
-            </div>           
-        </div>
+    
+    <div class="bigbox">
+    <section class="terminal">
         
-        <div id="terminal">
-            <div class="terminalbox2">
-                <h1> Select a Category</h1>
-            </div>
+        <div class="select-terminal">
+            Select a terminal
+            <button>Reset</button>
         </div>
-        
-    <!-- </section> -->
+        <div class="terminal-op">
+            <button>T1</button>
+            <button>T2</button>
+            <button>T3</button>
+            <button>T4</button>
+        </div>
+        <div class="select-terminal">
+        Select a Catagory
+        </div>
+        <div class="select-category">
+            <button>Family Favourits</button>
+            <button>Casual Dining</button>
+            <button>Coffee House & Cafe</button>
+            <button>International Cusine</button>
+        </div>
+    </section>
+    
 
         <section class="shop selection">
         <h1 class="h-primary"></h1>
@@ -95,29 +110,56 @@
         </div>
         </section>
     </section>
-
-    <hr>
-    <nav id="navbar2">
-        <div id="logo2">
-            <img src="../img/Restaurent_Pic/logo.png" alt="">
-              <!-- <h2>XTON Airport Limited</h2> -->
-            <hr>
+    </div>
+    
+    <footer>
+      <!-- First Footer Section -->
+      <div class="footer-1st">
+        <div class="footer-logo"></div>
+        <div class="title">XTON Airport</div>
+      </div>
+      <!-- Second Footer Section -->
+      <div class="footer-2nd">
+        <div class="footer2d-info">
+          <!-- Contact Information -->
+          <div class="info">
+            <p>Address</p>
+            <address>
+              <p>XTON Airport LTD.</p>
+              <p>Agargaon, Mirpur.</p>
+              <p>Dhaka, Bangladesh.</p>
+            </address>
+          </div>
+          <!-- Discover XTON -->
+          <div class="info">
+            Discover XTON
+            <p>Special Offer.</p>
+            <p>Where To Eat.</p>
+            <p>Where to Shop.</p>
+          </div>
+          <!-- Social Media Links -->
+          <div class="info">
+            <p>Contact us</p>
+            <p class="links">
+              <i class="fa-brands fa-facebook"></i>
+              <i class="fa-brands fa-square-x-twitter"></i>
+              <i class="fa-brands fa-square-instagram"></i>
+              <i class="fa-brands fa-youtube"></i>
+            </p>
+          </div>
         </div>
-
-        <div id="logo3">
-            <img src="../img/Restaurent_Pic/up-arrow.png" alt="">
-            
-        </div>
-        
-
-        <!-- <ul>
-            <li class="item"><a href="#">At the Airport</a></li>
-            <li class="item"><a href="#">Flights</a></li>
-            <li class="item"><a href="#">Booking</a></li>
-            <li class="item"><a href="#">Help</a></li>
-            <li class="item"><a href="#">Reward</a></li>
-        </ul> -->
-    </nav>
+      </div>
+      <!-- Last Footer Section -->
+      <div class="footer-last">
+        <p>privacy</p>
+        <p>Terms and Condition</p>
+        <p>Accessibility</p>
+        <p>Communication</p>
+        <p>Health and Safety</p>
+        <p>Help</p>
+        <p>&copy;XTON Airports Limited</p>
+      </div>
+    </footer>
     
 </body>
 </html>
