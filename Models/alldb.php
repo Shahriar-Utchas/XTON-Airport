8f7d01d62b061c6029fd1102b7141f3ae32655cf
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2024-05-01 20:54:14 +0600

    Update alldb.php

--- Models/alldb.php
+++ Models/alldb.php
@@ -28,4 +28,29 @@
             return false;
         }
     }
+
+    function get_fast_foods(){
+        $sql="select * from rest_foods where type='fastfood'";
+        $con=conn();
+        $res= mysqli_query($con,$sql);
+        return $res;
+    }
+    function get_lunch(){
+        $sql="select * from rest_foods where type='lunch'";
+        $con=conn();
+        $res= mysqli_query($con,$sql);
+        return $res;
+    }
+    function get_coffee(){
+        $sql="select * from rest_foods where type='coffee'";
+        $con=conn();
+        $res= mysqli_query($con,$sql);
+        return $res;
+    }
+    function get_interCousine(){
+        $sql="select * from rest_foods where type='interCousi'";
+        $con=conn();
+        $res= mysqli_query($con,$sql);
+        return $res;
+    }
 ?>
