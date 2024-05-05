250ed4e4a64bab2e0fbffaee9df9dcc1f260c9ca
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2024-05-05 18:40:35 +0600

    Update passengerHome.php

--- Views/passengerHome.php
+++ Views/passengerHome.php
@@ -19,6 +19,7 @@ $info = get_user_info($_SESSION['user']);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==
         " crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 <body>
     <div class="sidebar">
@@ -28,27 +29,62 @@ $info = get_user_info($_SESSION['user']);
         <div class="profile_pic">
             <?php  
                 while($row=mysqli_fetch_assoc($info)){
-                echo "<img src='data:;base64,".base64_encode($row['img'])."' alt='image' style='width: 120%; height: 200px;'>";
+                echo "<img src='data:;base64,".base64_encode($row['img'])."' alt='image' style='width: 100%; height: 160px;'>";
                 }
             ?>
         </div>
         <div class="name">
-            <p> Welcome <?php echo $_SESSION['user'];?></p>
+            <p> Welcome, <?php echo $_SESSION['user'];?></p>
         </div>       
         <div class="options">
-        <button id="Dashboard"><i class="fa-solid fa-chart-column"></i><a href="#">Home</a></button><br><br>
-        <button id="Dashboard"><i class="fa-solid fa-chart-column"></i><a href="#">Profile</a></button><br><br>
-        <button id="Dashboard"><i class="fa-solid fa-chart-column"></i><a href="#">Settings</a></button><br><br>
-        <button id="Dashboard"><i class="fa-solid fa-chart-column"></i><a href="#">Logout</a></button><br><br>
+        <button id="Home"><i class="fa-solid fa-house"></i>&nbsp;Home</button><br><br>
+        <button id="Dashboard"><i class="fa-solid fa-user"></i>&nbsp;Profile</button><br><br>
+        <button id="Dashboard"><i class="fa-solid fa-gear"></i>&nbsp;Settings</button><br><br>
+        <button id="out" onclick = "logout()"><i class="fa-solid fa-right-from-bracket"></i>&nbsp;Logout</button><br><br>
         </div>
 
     </div>
     <div class="navbar">
-        
+        <button id="Dashboard"><i class="fa-solid fa-house"></i>&nbsp;Home</button>
+
+        <div class="searchBar">
+          <input type="text" placeholder="search here" />
+          <div class="search-icon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+
     </div>
     <div class="content">
 
     </div>
+    <script>
+
+        function logout() {
+            swal({
+                    title: "Are you sure you want to logout?",
+                    icon: "warning",
+                    buttons: ["Cancel", "Logout"],
+                    dangerMode: true,
+                }).then((logout) => {
+                    if (logout) {
+                        // Make an AJAX request to logout.php
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("POST", "../Controllers/logout.php", true);
+                        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState == 4 && xhr.status == 200) {
+                                // Redirect to login page after successful logout
+                                window.location.href = "../Views/LoginRegistration.php";
+                            }
+                        };
+                        xhr.send();
+                    }
+                });
+        }
+
+       
+    </script>
     
 </body>
 </html>
